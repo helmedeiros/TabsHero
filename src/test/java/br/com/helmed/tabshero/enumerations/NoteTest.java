5ca110a4ecb40a4bdbf697f696cb2b396d9bb42c
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-07-08 21:40:50 -0300

    Adding notes.

---
+++ src/test/java/br/com/helmed/tabshero/enumerations/NoteTest.java
@@ -0,0 +1,21 @@
+package br.com.helmed.tabshero.enumerations;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+
+/**
+ * User: helmed
+ * Date: 7/8/13
+ * Time: 9:31 PM
+ */
+public class NoteTest {
+    @Test public void shouldReturnNullWhenNoNoteWasFound() throws Exception {
+        assertNull(Note.byItsLetter("W"));
+    }
+
+    @Test public void shouldReturnTheDONoteWhenCIsInformed() throws Exception {
+        assertEquals(Note.DO, Note.byItsLetter("C"));
+    }
+}
